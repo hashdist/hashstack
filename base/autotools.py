2d00b3198ef5f4644562b13610857ab6edada41f
Dag Sverre Seljebotn <d.s.seljebotn@astro.uio.no>
2013-09-23 21:04:48 +0200

    Python-constructed configure line with CFLAGS, LDFLAGS + -j-arg to make

--- base/autotools.py
+++ base/autotools.py
@@ -1,6 +1,39 @@
 from hashdist import build_stage
 
+def rpath_flag(ctx, path):
+   if ctx.parameters['platform'] == 'linux':
+       return '-Wl,-rpath=%s' % path
+
 @build_stage()
-def configure(ctx, stage):
-    lines = ['./configure --prefix="${ARTIFACT}"']
-    return lines
+def configure(ctx, stage_args):
+    """
+    Generates ./configure line.
+
+    Example::
+
+        - name: configure
+          extra: ['--enable-foo', '--with-zlib=${ZLIB_DIR}']
+          set_env_flags: true # default
+
+    If set_env_flags is set, CFLAGS and LDFLAGS will be set, as appropriate for the
+    platform.
+    """
+    conf_lines = ['./configure --prefix="${ARTIFACT}"']
+    if 'extra' in stage_args:
+        conf_lines.append(' '.join('"%s"' % arg for arg in stage_args['extra']))
+    for i in range(len(conf_lines) - 1):
+        conf_lines[i] = conf_lines[i] + ' \\'
+        conf_lines[i + 1] = '  ' + conf_lines[i + 1]
+
+    env_lines = []
+    if stage_args.get('set_env_FLAGS', True):
+        CFLAGS = []
+        LDFLAGS = []
+        for dep_var in ctx.dependency_dir_vars:
+            CFLAGS.append('-I${%s_DIR}/include' % dep_var)
+            LDFLAGS.append('-L${%s_DIR}/lib' % dep_var)
+            LDFLAGS.append(rpath_flag(ctx, '${%s_DIR}/lib' % dep_var))
+        env_lines.append('export CFLAGS="%s"' % ' '.join(CFLAGS))
+        env_lines.append('export LDFLAGS="%s"' % ' '.join(LDFLAGS))
+
+    return ['('] + env_lines + conf_lines + [')']
